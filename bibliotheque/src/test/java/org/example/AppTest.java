93401e91993a62f527278b296e9ab48519b91449
CelinePN <79644388+CelinePN@users.noreply.github.com>
2023-02-07 11:55:24 +0100

    instal

---
+++ bibliotheque/src/test/java/org/example/AppTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static junit.framework.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
