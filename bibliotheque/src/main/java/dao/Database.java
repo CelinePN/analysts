8d21bfe9c98616d62745ab1c3cb2063884423166
marinesable <79644795+marinesable@users.noreply.github.com>
2023-03-10 08:09:03 +0100

    languesokk

--- bibliotheque/src/main/java/dao/Database.java
+++ bibliotheque/src/main/java/dao/Database.java
@@ -16,8 +16,34 @@ public class Database {
         .addHeader("Content-Type", "application/json")
         .addHeader("Access-Control-Request-Headers", "*")
         .addHeader("api-key", "xALvC4U1PdzK3K5y48tsvdpQar51gpnLLmKiNPQU4t2wOt11TqbyQ1mAabx8wAi6")
-        .addHeader("Accept", "application/ejson")
         .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
+
+    public void getLanguages() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create("{\n" +
+                "      \"dataSource\": \"Cluster0\",\n" +
+                "      \"database\": \"book_analysts\",\n" +
+                "      \"collection\": \"bibliotheques_paris\",\n" +
+                "      \"pipeline\": [\n" +
+                "          {\n" +
+                "            \"$group\": {\n" +
+                "                \"_id\": \"$Langue\",\n" +
+                "                 \"Nombre d'exemplaires\": { \"$sum\": 1 }\n" +
+                "            }\n" +
+                "         }\n" +
+                "      ]\n" +
+                "  }", mediaType);
+        Request request = new Request.Builder()
+                .url("https://data.mongodb-api.com/app/data-moehb/endpoint/data/v1/action/aggregate")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Access-Control-Request-Headers", "*")
+                .addHeader("api-key", "xALvC4U1PdzK3K5y48tsvdpQar51gpnLLmKiNPQU4t2wOt11TqbyQ1mAabx8wAi6")
+                .build();
         Response response = client.newCall(request).execute();
         System.out.println(response.body().string());
     }
