b632e8b63e1e6f7d7381d2be511a054704476d8c
CelinePN <79644388+CelinePN@users.noreply.github.com>
2023-03-18 17:05:58 +0100

    Tests

--- bibliotheque/src/test/java/dao/DatabaseTest.java
+++ bibliotheque/src/test/java/dao/DatabaseTest.java
@@ -1,16 +1,25 @@
 package dao;
+import modele.parametre.Parametre;
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
-
+/*
     @Test
     public void testConstructeur(){
         Database d = new Database();
         assertEquals(new Database(), d, "Database");
     }
 
+    @Test
+    public void testgetLanguage() throws IOException {
+        Database d = new Database();
+        List<Parametre> l = d.getLanguages();
+        assertEquals(new Database().getLanguages(), l, "langue");
+    }
+*/
 }
