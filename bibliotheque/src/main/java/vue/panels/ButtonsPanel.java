673596e0f033341ccafc31eb0e18da98119d57dc
Mathilde1625 <79644310+Mathilde1625@users.noreply.github.com>
2023-04-19 18:29:19 +0200

    Correction du menu

--- bibliotheque/src/main/java/vue/panels/ButtonsPanel.java
+++ bibliotheque/src/main/java/vue/panels/ButtonsPanel.java
@@ -1,6 +1,10 @@
 package vue.panels;
 
 
+import modele.parametre.ParametreType;
+import modele.utils.SortBy;
+import modele.utils.TypeDeDocGrouping;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +13,7 @@ public class ButtonsPanel extends JPanel {
 
         // panel utilisé pour la frame menu et la frame mainWindow
         //tous les params possibles
-        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 
         JLabel label = new JLabel("Choisir entre les différentes possibilités");
@@ -17,37 +21,60 @@ public class ButtonsPanel extends JPanel {
         this.add(label);
 
 
-        JLabel labelDemande = new JLabel("Les demandes");
-        JLabel labelOffres = new JLabel("Les offres");
-        JLabel labelOD = new JLabel("Les liens entre offres et demandes");
-        //boutons des demandes
-        JRadioButton boutonDemande1 = new JRadioButton("Genres les plus empruntés");
-        JRadioButton boutonDemande2 = new JRadioButton("Auteurs les plus empruntés");
-        JRadioButton boutonDemande3 = new JRadioButton("Nombre d'emprunts des types de documents");
+        JLabel labelParametre = new JLabel("Paramètres");
+        JLabel labelType = new JLabel("Par type");
+        JLabel labelLimite = new JLabel("Limite");
+        JLabel labelShortBy = new JLabel("ShortBy");
+        JLabel labelTypeGraph = new JLabel("Type de graphique ");
 
-        //boutons des offres
-        JRadioButton boutonOffre1 = new JRadioButton("Types de documents possédés");
-        JRadioButton boutonOffre2 = new JRadioButton("Langues les plus proposées");
+        //Menu déroulant pour Parametre
+       // String[] param = {"Auteur", "Langue", "Type de document", "Genre"};
+        JComboBox<ParametreType> comboBox = new JComboBox<>(ParametreType.values());
+        //comboBox.setPreferredSize(new Dimension(200, 300));
+        comboBox.setSelectedIndex(0); // Option sélectionnée par défaut
 
-        //boutons des offres et demandes
-        JRadioButton boutonOD1 = new JRadioButton("Offres et demandes par genre et type de documents");
-        JRadioButton boutonOD2 = new JRadioButton("Types de documents les plus rentables");
+      // String[] type = {"Films", "BD", "Musiques", "Jeux","Livres","Revues","Autres"};
+       JComboBox<TypeDeDocGrouping> comboBox2 = new JComboBox<>(TypeDeDocGrouping.values());
+       comboBox2.setSelectedIndex(0); // Option sélectionnée par défaut
+
+        String[] limit = {"1", "5", "10", "20"};
+        JComboBox<String> comboBox3 = new JComboBox<>(limit);
+        comboBox3.setSelectedIndex(0); // Option sélectionnée par défaut
+
+        //String[] shortby = {"Total des Exemplaires", "Total des Prêts"};
+        JComboBox<SortBy> comboBox4 = new JComboBox<>(SortBy.values());
+        comboBox4.setSelectedIndex(0); // Option sélectionnée par défaut
+
+       JRadioButton BarreEmpilees = new JRadioButton("BarresEmpilees");
+       JRadioButton Camembert = new JRadioButton("Camembert");
+       JRadioButton Histogramme = new JRadioButton("Histogramme");
+        // Création du panel contenant la liste déroulante
+       /* JPanel selectionPanelParam = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 5));
+      */
+
         JButton valider = new JButton("Valider");
         valider.setSize(100,50);
 
-        this.add(labelDemande);
-        this.add(boutonDemande1);
-        this.add(boutonDemande2);
-        this.add(boutonDemande3);
 
-        this.add(labelOffres);
-        this.add(boutonOffre1);
-        this.add(boutonOffre2);
+        this.add(labelParametre);
+        this.add(comboBox);
 
-        this.add(labelOD);
-        this.add(boutonOD1);
-        this.add(boutonOD2);
-        this.add(valider);
+        this.add(labelType);
+        this.add(comboBox2);
+
+        this.add(labelLimite);
+        this.add(comboBox3);
+
+        this.add(labelShortBy);
+        this.add(comboBox4);
+
+        this.add(labelTypeGraph);
+        this.add(BarreEmpilees);
+       this.add(Camembert);
+       this.add(Histogramme);
+
+
+     this.add(valider);
 
        // this.add(this, BorderLayout.EAST);
     }
