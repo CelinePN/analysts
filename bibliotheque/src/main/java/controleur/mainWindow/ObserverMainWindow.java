162cd8a69a275e6639940d5bb2bb3a484c296802
Mathilde1625 <79644310+Mathilde1625@users.noreply.github.com>
2023-05-22 16:19:47 +0200

    Attention

--- bibliotheque/src/main/java/controleur/mainWindow/ObserverMainWindow.java
+++ bibliotheque/src/main/java/controleur/mainWindow/ObserverMainWindow.java
@@ -13,6 +13,8 @@ public interface ObserverMainWindow {
 
     void choisir();
 
+
+
     void updateGraphPanel(List<Parametre> liste, TypeGraph typeGraph, Mode currentMode);
 
     void updateGraphBarre(List<Parametre> listeexemplaire, List<Parametre> listeemprunt);
--- bibliotheque/src/main/java/vue/frames/MainWindow.java
+++ bibliotheque/src/main/java/vue/frames/MainWindow.java
@@ -251,4 +251,5 @@ public class MainWindow extends JFrame implements ObserverMainWindow {
 
     }
 
+
 }
